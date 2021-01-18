3ed18cf1adb0eac566bd269b6230e837e560978d
Marina Delgado <marinadelges@gmail.com>
2021-01-18 19:35:14 +0100

    add excersice

---
+++ modulo2/leccion1/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const mainTitle = document.querySelector('div');
+
+const content = '<h1><a href="http://via.placeholder.com/350x150">Lorem ipsum</a></h1> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit<p>';
+
+mainTitle.innerHTML = content;

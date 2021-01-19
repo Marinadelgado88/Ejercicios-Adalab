375c795c87c391e128e0aa07ba6862b08c3fcb71
Marina Delgado <marinadelges@gmail.com>
2021-01-19 18:38:07 +0100

    try to understand the console

--- modulo2/leccion1/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const mainTitle = document.querySelector('div');
-
-const content = '<h1><a href="http://via.placeholder.com/350x150">Lorem ipsum</a></h1> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit<p>';
-
-mainTitle.innerHTML = content;
---
+++ modulo2/leccion2/modulo2-leccion2-ejercicio2/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const superWave = document.querySelector('.super-Wave');
+superWave.innerHTML = 'Bola';

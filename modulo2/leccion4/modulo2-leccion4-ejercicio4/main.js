ae39817a7973c0af7ac5dbe05a00085bcf06fa89
Marina Delgado <marinadelges@gmail.com>
2021-01-21 17:17:29 +0100

    add excercise

---
+++ modulo2/leccion4/modulo2-leccion4-ejercicio4/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function paresONones(a){
+    return (a%2)
+}
+if(paresONones(8)===0){
+    document.write("Es un número par");
+}else{
+    document.write("Es un número impar");
+}
+

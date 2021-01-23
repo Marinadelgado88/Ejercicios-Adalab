b0e3721d63805c74055c44a395e4925479df21a7
Marina Delgado <marinadelges@gmail.com>
2021-01-23 10:24:29 +0100

    add excercise

---
+++ modulo2/leccion5/modulo2-leccion5-ejercicio1/main.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.click');
+function showclick() {
+    console.log('Mi primer click, ¡ole yo y la mujer que me parió!');
+  }
+  
+  button.addEventListener('click', showclick);

13935d96554d016fb9ca6ffe05b8692ac439db4a
Marina Delgado <marinadelges@gmail.com>
2021-01-26 21:29:52 +0100

    add excercise

---
+++ modulo2/leccion7/modulo2-leccion7-ejercicio1/main.js
@@ -0,0 +1,10 @@
+function workWithMovies (){
+
+const movies = ['Big fish', 'Dirty dancing', 'El show de Truman'];
+movies[3] = 'Peters friends';
+movies [3] = 'El sexto sentido';
+movies [0] = 'Peters friends';
+
+console.log(movies);
+};
+workWithMovies();

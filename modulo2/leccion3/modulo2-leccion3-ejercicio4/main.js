e7f9856cb7da04b8eeb96caa3189597d8737ab57
Marina Delgado <marinadelges@gmail.com>
2021-01-28 20:31:22 +0100

    add excercise

---
+++ modulo2/leccion3/modulo2-leccion3-ejercicio4/main.js
@@ -0,0 +1,17 @@
+const ageConversor =document.querySelector('.conversor');
+const ageDog = parseInt(ageConversor.innerHTML);
+
+if (ageDog === 1){
+    ageConversor.innerHTML = "Tu perro tiene 15 años de humanos" 
+
+} else if (ageDog === 2){
+    ageConversor.innerHTML = "Tu perro tiene 9 años" 
+
+} else {
+    ageConversor.innerHTML = "Tu perro tiene 5 años de humanos" 
+    
+}
+
+
+
+

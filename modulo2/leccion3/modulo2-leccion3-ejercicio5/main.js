a69feb112c4f37b750fde66c9a8d6cc9b22e6f1d
Marina Delgado <marinadelges@gmail.com>
2021-01-28 21:13:02 +0100

    add excercise

--- modulo2/leccion3/modulo2-leccion3-ejercicio5/main.js
+++ modulo2/leccion3/modulo2-leccion3-ejercicio5/main.js
@@ -1,4 +1,21 @@
 'use strict';
 const box = document.querySelector(".container");
-const title = document.querySelector(".title");
-const info = document.querySelector(".info");
+const tittle =document.querySelector('.tittle');
+const text = document.querySelector('.info');
+let method = prompt("Introduce que clase quieres, succes , error o warning").toLowerCase();
+
+if( method === 'warning'){
+    box.classList.add ('warning')
+    tittle.innerHTML = 'AVISO';
+    text.innerHTML ='Tenga cuidado'
+} else if (method ==='error') {
+    box.classList.add ('error')
+        tittle.innerHTML = 'ERROR';
+        text.innerHTML ='Ha surgido un error'
+} else if (method === 'succes') {
+    box.classList.add ('succes')
+        tittle.innerHTML = 'CORRECTO';
+        text.innerHTML ='Los datos son correctos'
+}
+
+
